cbddce94a87a3992cd1c13ba996d1b9997fb1e67
Jonatan Cardoso <jonatancardosoc@gmail.com>
2024-02-15 15:31:06 -0300

    Fix responsive

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+'use client'
+import * as React from 'react';
+import { useState } from 'react'
 import styles from './Layout.module.css'
-import { Button, Container, Stack } from '@mui/material';
+import { Box, Button, Container, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack } from '@mui/material';
 import Image from 'next/image'
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 interface NavLink {
     name: string,
@@ -14,7 +20,67 @@ const navLinks: NavLink[] = [
     { name: 'Inicio', href: '#' }
 ]
 
+type Anchor = 'left';
+
 export function Layout () {
+    useState(() => {
+        console.log('in useState')
+    });
+
+    const [state, setState] = React.useState({
+        left: false,
+    });
+    
+    const toggleDrawer =
+        (anchor: Anchor, open: boolean) =>
+        (event: React.KeyboardEvent | React.MouseEvent) => {
+            if (
+                event.type === 'keydown' &&
+                ((event as React.KeyboardEvent).key === 'Tab' ||
+                (event as React.KeyboardEvent).key === 'Shift')
+            ) {
+                return;
+            }
+        
+            setState({ ...state, [anchor]: open });
+        };
+    
+    const list = (anchor: Anchor) => (
+        <Box
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    const isMobile = useMediaQuery('(max-width: 960px)');
+
     return (
         <main className={styles.main}>
             <Container className={styles.container}>
@@ -34,6 +100,24 @@ export function Layout () {
                         </Button>
                     ))}
                 </Stack>
+                <div>
+                    {(['left'] as const).map((anchor) => (
+                        <React.Fragment key={anchor}>
+                        {isMobile && (
+                            <Button onClick={toggleDrawer(anchor, true)}>
+                                <MenuIcon sx={{color: '#01a94e'}} />    
+                            </Button>
+                        )}
+                        <Drawer
+                            anchor={anchor}
+                            open={state[anchor]}
+                            onClose={toggleDrawer(anchor, false)}
+                        >
+                            {list(anchor)}
+                        </Drawer>
+                        </React.Fragment>
+                    ))}
+                </div>
             </Container>
         </main>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import styles from "./page.module.css";
 import { Layout } from "./components/Layout"
 import { Carousel } from "./components/Carousel"
 import { SobreNos } from "./components/SobreNos"
+import { Servicos } from './components/Servicos';
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <div className={styles.description}>
           <Layout />
           <Carousel />
           <SobreNos />
+          <Servicos />
       </div>
     </main>
   );
